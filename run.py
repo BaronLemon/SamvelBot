cb075cb2bc33ba8716b9c818615368df59b383ec
BaronLemon <daniil.sabitov@list.ru>
2025-05-06 22:01:32 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,28 @@
 import logging
+import os
 from aiogram import Bot, Dispatcher, F
 from app.handlers.base import rt
 from config import TOKEN
 from app.schedule import check_subscriptions
+from dotenv import load_dotenv
 import asyncio
 
+load_dotenv()
 
-bot_api = TOKEN
-
-
-bot = Bot(token=bot_api)
-dp = Dispatcher()
-dp.include_router(rt)
-
-
+bot_api = os.getenv('TOKEN')
 
 async def main():
-    asyncio.create_task(check_subscriptions(bot))
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    async with Bot(token=bot_api) as bot:
+        dp = Dispatcher()
+        dp.include_router(rt)
 
+        try:
+            asyncio.create_task(check_subscriptions(bot))
+            await bot.delete_webhook(drop_pending_updates=True)
+            await dp.start_polling(bot)
+
+        finally:
+            print('Bot is closed')
 
 if __name__ == '__main__':
 
