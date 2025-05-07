3d8153167a9679dca7d46ca1eedaa902e0914183
BaronLemon <daniil.sabitov@list.ru>
2025-05-07 22:00:53 +0300

    Update func.py

--- app/func.py
+++ app/func.py
@@ -1,18 +1,42 @@
 from aiogram.types import LabeledPrice, CallbackQuery
+import json
 
-payment_api = 'payment_API'
+payment_api = '390540012:LIVE:69817'
+
 
 
 async def send_invoice(callback: CallbackQuery):
-    price = [LabeledPrice(label='Месячная подписка', amount=50000)]
+    price = [LabeledPrice(label='Месячная подписка', amount=500 * 100)]
+    provider_data = json.dumps({
+    "receipt": {
+        "items": [
+        {
+            "description": f"User ID: {callback.from_user.id}",
+            "quantity": 1,
+            "amount": {
+              "value": 500,
+              "currency": "RUB"
+            },
+            "vat_code": 1,
+            "payment_mode": "full_payment",
+            "payment_subject": "service"
+        }
+        ]
+    }
+    })
+
+
     await callback.message.answer_invoice(
         title = 'Подписка на канал',
         description='Месячная подписка на закрытый тгк',
-        prices = price, 
+        provider_token=payment_api,
+        currency='RUB', 
         need_email=True,
         send_email_to_provider=True,
-        provider_token=payment_api,
-        payload='channel_sub',
-        currency='RUB'
+        is_flexible=False,
+        prices= price,
+        start_parameter='channel_sub',
+        payload=f'{callback.from_user.id}',
+        provider_data=provider_data
     )
 
